4d56fd525ee64a0799b1610d0cfd65a39989776b
mark-solo <clueless.magician@gmail.com>
2019-04-21 12:50:28 +0300

    base, index and _ticket templates

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,4 +5,15 @@ from app.main import bp
 @bp.route('/', methods=['GET'])
 @bp.route('/index', methods=['GET'])
 def index():
-	return render_template('index.html')
+	tickets = [
+		{
+			'author': 'pew',
+			'title': 'some thing',
+			'body': 'lorem ipsum etc etc'
+		},
+		{
+			'author': 'anon',
+			'body': 'it\'s only water, it\'s onle fire, it\'s only love'
+		},
+	]
+	return render_template('index.html', tickets=tickets)
